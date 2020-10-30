4e04eaa1e1d22e462d70c8eece7a7ad73158e925
Robert Aube <robert.aube@cegeplimoilou.ca>
2020-10-30 14:05:29 -0400

    Nouvelle mise à jour des commentaires et du script push

--- pushAll.bat
+++ pushAll.bat
@@ -2,8 +2,11 @@
 if [%1]==[] goto Erreur
 set all="%*"
 @ECHO ON
+echo ************
 git add *
+echo ************
 git commit -m %all%
+echo ************
 git push -u origin --all
 @ECHO OFF
 GOTO Fin
@@ -11,6 +14,8 @@ GOTO Fin
 echo Indiquez en argument la raison de la mise à jour
 echo Ne pas mettre d'accent et de guillemets! 
 echo Exemple:
-echo .\pushAll.bat Nouvelle mise à jour des commentaires et du script push
+echo pushAll.bat Nouvelle mise à jour des commentaires et du script push
 :Fin
+echo ************
+git status
 PAUSE
