578ee2a8960adb023a34ff9fcbd7336e23400367
Robert Aubé <robert.aube@cegeplimoilou.ca>
2020-11-13 11:58:54 -0500

    MAJ Cours G2 ajout d'explications dans le code.

--- 126 - ra - g2/PrjDemosOO/src/Rectangle.java
+++ 126 - ra - g2/PrjDemosOO/src/Rectangle.java
@@ -1,23 +1,22 @@
 public class Rectangle {
-
     //public = accessibilité possible à l'extérieur de la classe
     //static = n'a pas pas besoin d'être instancié pour l'utiliser. (Pas besoin de faire un new) 1 et 1 seul par classe.
-    //final = constante ne peut pas être modifié.
+    //final = constante ne peut pas être modifiée et obligation de définir sur le champ.
     public static final int DEFAULT_HAUTEUR = 0;
     public static final int DEFAULT_LARGEUR = 0;
 
-    //attribut d'un rectangle (variable d'instance) Ces variables existe seulement si j'ai un objet ou instance
-    private int hauteur;   //on les met de visibilité private pour qu'elle soit encapsulé (Non accessible à l'Extérieur de la classe)
+    //attributs d'un rectangle (variables d'instance) Ces variables existent seulement si j'ai un objet ou instance
+    private int hauteur;   //on les met de visibilité private pour qu'elle soit encapsulée (Non accessible à l'extérieur de la classe)
     private int largeur;
 
     //contructeur par défaut
     public Rectangle() {
-        this(DEFAULT_HAUTEUR, DEFAULT_HAUTEUR); //ici this est utilisé pour appeler un autre constructeur. Doit être la première instruction.
+        this(DEFAULT_HAUTEUR, DEFAULT_HAUTEUR); //ici this est utilisé pour appeler un autre constructeur. Doit être la première instruction du constructeur.
         System.out.println("Rectangle() - constructeur par défaut - " + toString());
     }
 
     public Rectangle(int hauteur, int largeur) {
-        setLargeur(largeur);
+        setLargeur(largeur); //pour être cohérant, on doit passer par les setters pour valider...
         setHauteur(hauteur);
     }
 
@@ -27,7 +26,7 @@ public class Rectangle {
         return air;
     }
 
-    //accesseur (getteur)
+    //accesseur (getter)
     public int getHauteur() {
         return hauteur;
     }
@@ -36,7 +35,7 @@ public class Rectangle {
     public void setHauteur(int hauteur) {
         if (!estHauteurValide(hauteur))
             throw new IllegalArgumentException("Hauteur invalide : " + hauteur);
-        this.hauteur = hauteur;
+        this.hauteur = hauteur; //this fait référence à l'attribut l'instance courante.
     }
 
     public int getLargeur() {
@@ -49,18 +48,23 @@ public class Rectangle {
         this.largeur = largeur; //this fait référence à l'attribut l'instance courante.
     }
 
+    //Cette méthode est static: Donc je n'ai pas besoin d'instanciation pour y accéder.
+    //Cependant, une méthode static n'as pas accès aux attributs. C'est normal: je n'ai pas besoin d'instance pour les utiliser.
     public static boolean estLargeurValide(int largeur) {
         boolean estValide;
         estValide = largeur >= 0;
         return estValide;
     }
 
+    //Cette méthode est static: Donc je n'ai pas besoin d'instanciation pour y accéder.
+    //Cependant, une méthode static n'as pas accès aux attributs. C'est normal: je n'ai pas besoin d'instance pour les utiliser.
     public static boolean estHauteurValide(int hauteur) {
         boolean estValide;
         estValide = hauteur >= 0;
         return estValide;
     }
 
+    //La méthode toString() est normalisé et est spécicialement conçu pour retourner une String qui présent l'objet instancié.
     @Override
     public String toString() {
         String s = "";
@@ -71,4 +75,5 @@ public class Rectangle {
         return s;
     }
 
+    //pas de main() ici. c'est normal, le main n'a aucun rapport avec un Rectangle.
 }
