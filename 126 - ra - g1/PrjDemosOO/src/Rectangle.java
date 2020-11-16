909822706155fb52811cc4af85ec2e9e0ee36cba
Robert Aubé <robert.aube@cegeplimoilou.ca>
2020-11-16 12:58:33 -0500

    MAJ Cours 20 G1 parte 2

--- 126 - ra - g1/PrjDemosOO/src/Rectangle.java
+++ 126 - ra - g1/PrjDemosOO/src/Rectangle.java
@@ -1,7 +1,22 @@
 public class Rectangle {
-    //attributs d'un rectangles (variables d'instance), Ces variables existent seulement si j'ai un objet ou instance
+    //public = accessibilité possible à l'extérieur de la classe
+    //static = existe même sans instance (ou objet) 1 et 1 seul classe;
+    //final = constante ne peut pas être modifiée.
+    public static final int DEFAULT_HAUTEUR = 0;
+    public static final int DEFAULT_LARGEUR = 0;
+    //attributs d'un rectangle (variables d'instance). Ces variables existent seulement si j'ai un objet ou instance
     private int hauteur; //on met une visiblité private pour encapsuler ces données (Non accessivble à l'extérieur de la classe)
     private int largeur;
+
+    public Rectangle() {
+        this(DEFAULT_HAUTEUR, DEFAULT_LARGEUR); //ici this est utilisé pour appeler un autre contructeur. Doit être la première instruction du constructeur
+       // System.out.println("Rectangle - coustructeur par défaut");
+    }
+
+    public Rectangle(int hauteur, int largeur) {
+        setHauteur(hauteur);
+        setLargeur(largeur);
+    }
 
     public int getHauteur() {
         return hauteur;
@@ -10,12 +25,13 @@ public class Rectangle {
     public void setHauteur(int hauteur) {
         if (hauteurEstValide(hauteur)) {
             this.hauteur = hauteur;  //this fait référence à l'objet courant
-        }
-        else
+        } else
             throw new IllegalArgumentException("Hauteur invalide : " + hauteur);
     }
 
-    private boolean hauteurEstValide(int hauteur) {
+    //Cette méthode est static : Donc, je n'ai pas besoin d'instance pour l'utiliser.
+    //Cette méthode n'a pas accès aux attributs. C'est normal: je n'ai pas besoin d'instance pour l'utiliser.
+    public static boolean hauteurEstValide(int hauteur) {
         boolean estValide;
         estValide = hauteur >= 0;
         return estValide;
@@ -28,14 +44,25 @@ public class Rectangle {
     public void setLargeur(int largeur) {
         if (largeurEstValide(largeur)) {
             this.largeur = largeur;
-        }
-        else
+        } else
             throw new IllegalArgumentException("Largeur invalide : " + largeur);
     }
 
-    private boolean largeurEstValide(int largeur) {
+    public static boolean largeurEstValide(int largeur) {
         boolean estValide;
         estValide = largeur >= 0;
         return estValide;
     }
+
+    @Override
+    public String toString() {
+        String s;
+        s = "Rectangle{" +
+                "hauteur=" + hauteur +
+                ", largeur=" + largeur +
+                '}';
+        return s;
+    }
+
+    //pas de main() ici. C'est normal, le main n'a aucun rapport avec un Rectangle.
 }
--- 126 - ra - g1/PrjDemosOO/src/RectangleTest.java
+++ 126 - ra - g1/PrjDemosOO/src/RectangleTest.java
@@ -1,14 +1,61 @@
 public class RectangleTest {
     public RectangleTest() {
 
+        //       testerGetSet();
+        //   testerToString();
+        //  testerVariableFinal();
+        testerValiderHauteurLargeur();
+
+    }
+
+    private void testerValiderHauteurLargeur() {
+        Rectangle r1;
+        int hauteur;
+
+        hauteur = -2;
+
+
+        if (Rectangle.hauteurEstValide(hauteur)) {
+            r1 = new Rectangle(hauteur, 0);
+            System.out.println(r1);
+        }
+    }
+
+    private void testerVariableFinal() {
+        int a;
+
+        a = Rectangle.DEFAULT_HAUTEUR;
+
+//        System.out.println(Math.PI);
+
+        //    Rectangle.DEFAULT_HAUTEUR = 3;  //impossible c'Est une constante.
+
+    }
+
+    private void testerToString() {
         Rectangle r1 = new Rectangle();
-        Rectangle r2 = new Rectangle();
+        Rectangle r2 = new Rectangle(14, 33);
+
+        System.out.println(r2);
+        System.out.println(r1.toString());
+    }
+
+    private void testerGetSet() {
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Rectangle(14, 33);
+
+        System.out.println(r1.getHauteur() + ", " + r1.getLargeur());
+        System.out.println(r2.getHauteur() + ", " + r2.getLargeur());
 
         r1.setHauteur(3);
         r1.setLargeur(5);
 
-      //  r2.setHauteur(-4); //lance une IllegalArgumentException
-      //  r2.setLargeur(-2); //lance une IllegalArgumentException
+        r2.setHauteur(2);
+        r2.setLargeur(7);
+
+
+        //  r2.setHauteur(-4); //lance une IllegalArgumentException
+        //  r2.setLargeur(-2); //lance une IllegalArgumentException
 
         System.out.println(r1.getHauteur() + ", " + r1.getLargeur());
         System.out.println(r2.getHauteur() + ", " + r2.getLargeur());
